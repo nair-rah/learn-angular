3a4857654217f5266af92e38aac55c32987faded
nair-rah <nair.rahul200798@gmail.com>
2022-10-08 23:17:35 +0530

    Basics Of Angular covered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'learn-angular';
   isDisabled: boolean = false;
+
   toggleColor: boolean = false;
   color: string = '#FFFFFF';
+
   warningMessages: string[] = [];
-  successMessages: string[] = []
+  successMessages: string[] = [];
+
+  isMessageToBeDisplayed: boolean = true;
+  timestampLog: string[] = [];
 
   // Runs on reset name button click
   resetTitleOnClick(): void {
@@ -30,10 +35,18 @@ export class AppComponent {
     this.toggleColor ? (this.color = 'lightpink') : (this.color = '#FFFFFF');
   }
 
+  // Adds element in warning messages array on button click
   addWarning() {
     this.warningMessages.push('xxx');
   }
+  // Adds element in success messages array on button click
   addSuccess() {
     this.successMessages.push('yyy');
+  }
+
+  toggleDisplayMessage(): void {
+    let date: Date = new Date();
+    this.timestampLog.push(date.toUTCString());
+    this.isMessageToBeDisplayed = !this.isMessageToBeDisplayed;
   }
 }
